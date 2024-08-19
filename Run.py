caffc3c5691c8f84e1adc5673461dab909d5d2fd
Adam <12666173+phoenixburn119@users.noreply.github.com>
2024-08-19 13:48:43 -0600

    Began implementing foreign keys
    Created a table and method to begin using foreign keys.

--- Run.py
+++ Run.py
@@ -7,18 +7,20 @@ from Sources.RFClass import *
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'Sources\ILOSourceSingleTest.csv')
 ILOSheet = pd.read_csv(filename)
-Client = DBClient()
-Client.Create_Database_Tables()
+DClient = DBClient()
+DClient.Create_Database_Tables()
+for host in range(ILOSheet.iloc[:,1].count()):
+    if(ILOSheet.iloc[host,0] != "Hostname"):
+        DClient.Write_HostListDatabase(ILOSheet.iloc[host,0])
 
 for i in range(ILOSheet.iloc[:,1].count()):
     if(ILOSheet.iloc[i,2] != "IP"):
-        # FClient = RFClient(ILOSheet.iloc[i,3], ILOSheet.iloc[i,4], ILOSheet.iloc[i,2])
-        # FClient.Gather_HostDisks(ILOSheet.iloc[i,0])
+        FClient = RFClient(ILOSheet.iloc[i,3], ILOSheet.iloc[i,4], ILOSheet.iloc[i,2])
+        FClient.Gather_HostDisks(ILOSheet.iloc[i,0])
         pass
 
 # Get_DiskDatabase('Hosts')
 
-DClient = DBClient()
-# DClient.Get_DiskDatabase('Full')
-DClient.Get_DiskByID('3')
+DClient.Get_DiskDatabase('Full')
+# DClient.Get_DiskByID('3')
 
--- Sources/DBClass.py
+++ Sources/DBClass.py
@@ -12,13 +12,31 @@ class DBClient:
         self.DBConnection.close()
 
     def Create_Database_Tables(self):
-        if not(os.path.isfile(os.path.join((os.path.dirname(__file__)), 'RedDataBase.db'))):
-            self.cursor.execute("CREATE TABLE IF NOT EXISTS HostDisks (id integer primary key, DateEntryAdded, Hostname, DriveID, SerialNumber, Model, HealthStatus, CapacityGB, CurrentTemperatureCelsius, MaximumTemperatureCelsius, Description, DiskDriveStatusReasons, FirmwareVersion, InterfaceSpeedMbps, InterfaceType, Location, MediaType, PowerOnHours, RotationalSpeedRpm, UncorrectedReadErrors, UncorrectedWriteErrors)")
-            # cur.execute("CREATE TABLE IF NOT EXISTS HostData (id integer primary key, Date Entry Added, )")
-            self.DBConnection.commit()
-            return("Creating Database")
-        else:
-            return("Database Already Exists")
+        # if not(os.path.isfile(os.path.join((os.path.dirname(__file__)), 'RedDataBase.db'))):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS HostIDList (id integer primary key, DateEntryAdded text CURRENT_TIMESTAMP, Hostname TEXT type UNIQUE)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS HostDisks (id integer primary key, DateEntryAdded, Hostname, DriveID, SerialNumber, Model, HealthStatus, CapacityGB, CurrentTemperatureCelsius, MaximumTemperatureCelsius, Description, DiskDriveStatusReasons, FirmwareVersion, InterfaceSpeedMbps, InterfaceType, Location, MediaType, PowerOnHours, RotationalSpeedRpm, UncorrectedReadErrors, UncorrectedWriteErrors)")
+        # self.cursor.execute("CREATE TABLE IF NOT EXISTS HostChassis (id integer primary key, DateEntryAdded)")
+        # self.cursor.execute("CREATE TABLE IF NOT EXISTS HostBIOS (id integer primary key, DateEntryAdded)")
+        # self.cursor.execute("CREATE TABLE IF NOT EXISTS HostSystem (id integer primary key, DateEntryAdded)")
+        # self.cursor.execute("CREATE TABLE IF NOT EXISTS HostNetwork (id integer primary key, DateEntryAdded)")
+        # self.cursor.execute("CREATE TABLE IF NOT EXISTS HostFans (id integer primary key, DateEntryAdded)")
+        # self.cursor.execute("CREATE TABLE IF NOT EXISTS HostThermal (id integer primary key, DateEntryAdded)")
+        # self.cursor.execute("CREATE TABLE IF NOT EXISTS HostPower (id integer primary key, DateEntryAdded)")
+        self.DBConnection.commit()
+        return("Creating Database")
+        # else:
+        #     return("Database Already Exists")
+        
+    def Write_HostListDatabase(self, Hostname: str) -> str:
+        try:
+            with self.DBConnection:
+                self.cursor.execute("INSERT INTO HostIDList(Hostname) VALUES (?)", [Hostname])
+        except:
+            print(f"Hostname {Hostname} could not be added as it was a duplicate entry.")
+        for row in self.DBConnection.execute("SELECT * FROM HostIDList"):
+                print(row)
+        
+            
 
     def Write_DiskDatabase(self, DiskData: dict, Hostname: str):
         InsertData = (Hostname, (DiskData['Id']), (DiskData['SerialNumber']), (DiskData['Model']), (DiskData['Status']['Health']), (DiskData['CapacityGB']), (DiskData['CurrentTemperatureCelsius']), (DiskData['MaximumTemperatureCelsius']), (DiskData['Description']), (''.join(str(x) for x in DiskData['DiskDriveStatusReasons'])), (DiskData['FirmwareVersion']['Current']['VersionString']), (DiskData['InterfaceSpeedMbps']), (DiskData['InterfaceType']), (DiskData['Location']), (DiskData['MediaType']), (DiskData['PowerOnHours']), (DiskData['RotationalSpeedRpm']), (DiskData['UncorrectedReadErrors']), (DiskData['UncorrectedWriteErrors']))
@@ -29,7 +47,6 @@ class DBClient:
         pass
     
     def Get_DiskDatabase(self, SearchMethod: str) -> str:
-        # cursor = database.cursor()
         print(type(SearchMethod))
         if(SearchMethod == 'Full'):
             for row in self.DBConnection.execute("SELECT * FROM HostDisks ORDER BY DateEntryAdded DESC"):
